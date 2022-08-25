592e5160b7aecd38027930536ffe2aa787869fbd
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-25 16:27:14 -0500

    got favs working

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,6 +1,31 @@
-function Favorites(){
-  return(
-    <h1>Favorites</h1>
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import FavoritesItem from "../FavoritesItem/FavoritesItem";
+
+function Favorites() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: 'GET_GIF'
+    })
+  }, [])
+
+  const giphyFavs = useSelector((store) => store.giphyFavs);
+
+
+  return (
+    <>
+      <h1>Favorites</h1>
+      <ul>
+        {giphyFavs.map(gif => (
+          <li key={gif.id}>
+            <FavoritesItem gif={gif}/>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
