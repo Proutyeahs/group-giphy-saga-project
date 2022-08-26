34985e0b56319eef4ce8e79f8f7acd7304f98854
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-26 09:53:42 -0500

    added back

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
+import { useHistory } from "react-router-dom";
 import FavoritesItem from "../FavoritesItem/FavoritesItem";
 import './Favorites.css'
 
 function Favorites() {
 
+  const history = useHistory();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,14 +17,18 @@ function Favorites() {
 
   const giphyFavs = useSelector((store) => store.giphyFavs);
 
-
+  const handleBack = () => {
+    console.log('SEND TO PREVIOUS PAGE');
+    history.push('/');
+  };
   return (
     <>
       <h1>Favorites</h1>
+      <span className="right"><button onClick={handleBack}>Back</button></span>
       <div className="listDiv">
         {giphyFavs.map(gif => (
           <div key={gif.id} className="imageDiv">
-            <FavoritesItem gif={gif}/>
+            <FavoritesItem gif={gif} />
           </div>
         ))}
       </div>
