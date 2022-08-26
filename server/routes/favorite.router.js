354ed145b5934da1d2e9226913c1b6d0cc349920
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-26 10:27:11 -0500

    delete

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -44,8 +44,14 @@ router.put('/:favId', (req, res) => {
 });
 
 // delete a favorite
-router.delete('/', (req, res) => {
-  res.sendStatus(200);
+router.delete('/:id', (req, res) => {
+  const queryText = 'DELETE FROM "image" WHERE id=$1;';
+  pool.query(queryText, [req.params.id])
+  .then(() => {res.sendStatus(200);})
+  .catch((err) => {
+    console.error('Error in delete', err)
+    res.sendStatus(500);
+  })
 });
 
 module.exports = router;
--- src/components/FavoritesItem/FavoritesItem.jsx
+++ src/components/FavoritesItem/FavoritesItem.jsx
@@ -1,8 +1,20 @@
+import { useDispatch } from "react-redux"
+
 function FavoritesItem({gif}) {
+
+    const dispatch = useDispatch();
+
+    const remove = (id) => {
+        dispatch({
+            type: 'DELETE_GIF',
+            payload: id
+        })
+    }
 
     return(
         <>
             <img src={gif.url} />
+            <button onClick={() => remove(gif.id)}>Delete</button>
         </>
     )
 }
