e13feb6e8ced8ee75311863d8b12e9703b55a095
AJ Timek <Timek612@gmail.com>
2022-08-26 13:12:29 -0500

    got app working again

--- src/components/FavoritesItem/FavoritesItem.jsx
+++ src/components/FavoritesItem/FavoritesItem.jsx
@@ -5,34 +5,28 @@ function FavoritesItem({ gif }) {
 
     const dispatch = useDispatch()
 
-    const dropDownChange = (cat) => {
-        console.log(cat);
-        dispatch ({
-            type: 'SET_CAT',
-            payload: {id: gif.id, category: cat}
-        })
-    }
+    // const dropDownChange = (cat) => {
+    //     console.log(cat);
+    //     dispatch({
+    //         type: 'SET_CAT',
+    //         payload: { id: gif.id, category: cat }
+    //     })
+    // }
 
-    return (
-        <>
-            <img src={gif.url} />
-            <label htmlFor="category">Select a category</label>
+    // return (
+    //     <>
+    //         <img src={gif.url} />
+    //         <label htmlFor="category">Select a category</label>
 
-            <select name="category" id="category">
-                <option value=""></option>
-                <option  onChange={() => dropDownChange(1)} value="Funny">Funny</option>
-                <option value="Cohort">Cohort</option>
-                <option value="Cartoon">Cartoon</option>
-                <option value="NSFW">NSFW</option>
-                <option value="Meme">Meme</option>
-            </select>
+    //         <select name="category" id="category">
+    //             <option value=""></option>
+    //             <option  onChange={() => dropDownChange(1)} value="Funny">Funny</option>
+    //             <option value="Cohort">Cohort</option>
+    //             <option value="Cartoon">Cartoon</option>
+    //             <option value="NSFW">NSFW</option>
+    //             <option value="Meme">Meme</option>
+    //         </select>
 
-
-
-
-function FavoritesItem({gif}) {
-
-    const dispatch = useDispatch();
 
     const remove = (id) => {
         dispatch({
@@ -41,7 +35,7 @@ function FavoritesItem({gif}) {
         })
     }
 
-    return(
+    return (
         <>
             <img src={gif.url} />
             <Button variant="contained" color="secondary" onClick={() => remove(gif.id)}>Delete</Button>
